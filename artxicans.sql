64dade3cd1b47ad37fdfafe1b7b20c96c1da312f
Nahuatlato <aztoock@gmail.com>
2023-07-01 18:38:11 -0600

    modifico reg-vendedores.php consulta sql solo muestra estatus pendiente

--- artxicans.sql
+++ artxicans.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-07-2023 a las 02:03:30
+-- Tiempo de generación: 02-07-2023 a las 02:37:41
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
