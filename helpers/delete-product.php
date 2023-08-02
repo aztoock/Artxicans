6570ae18182d285b3897610fec9930f9621e3e33
Nahuatlato <aztoock@gmail.com>
2023-08-01 18:34:24 -0600

    agrego cambio a delete-product.php

--- helpers/delete-product.php
+++ helpers/delete-product.php
@@ -6,10 +6,14 @@
       } 
           
       $id_product = $_GET['id'];
+        # deshabilito las restriccciones de clave FK
+      mysqli_query($conn, "SET FOREIGN_KEY_CHECKS = 0");
       # Eliminamos el producto que coincida con el id que se esta solicitando eliminar
       mysqli_query($conn,"DELETE FROM products WHERE id_product = '$id_product'");
       # De igual forma eliminamos todos los comentarios que contenga el producto.
       mysqli_query($conn,"DELETE FROM stars WHERE id_product = $id_product"); 
+        # habilito las restriccciones de clave FK
+      mysqli_query($conn, "SET FOREIGN_KEY_CHECKS = 1");
       echo("<script>location.href = '../productList.php';</script>");  
       
 ?> 
